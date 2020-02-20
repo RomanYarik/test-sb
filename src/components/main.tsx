67ca0b64981df08e2004c5ff5c1fad934951b2e8
RomanYar <romany@wix.com>
2020-02-20 13:20:39 +0200

    adding stylable

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,8 @@
-import './main.css'
-import React, { ReactNode } from 'react';
+import {classes} from './main.st.css'
+import React from 'react';
 
 export interface IMainProps  {
     text: string;
 }
 
-export const Main: React.FC<IMainProps> = ({children, text}) => <div><h1>{text}</h1><div className='content'>{children}</div></div>
+export const Main: React.FC<IMainProps> = ({children, text}) => <div><h1>{text}</h1><div className={classes.content}>{children}</div></div>
